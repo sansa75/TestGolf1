a450bd211291b175ce03dcf1f73562d6d4a4115d
song <bio950327@gmail.com>
2021-11-07 01:44:25 +0900

    FIREBASE 기반 확립

---
+++ app/src/main/java/com/example/testgolf1/LoginActivity.java
@@ -0,0 +1,4 @@
+package com.example.testgolf1;
+
+public class LoginActivity {
+}
--- app/src/main/java/com/example/testgolf1/MainActivity.kt
+++ app/src/main/java/com/example/testgolf1/MainActivity.kt
@@ -163,4 +163,5 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         super.onDestroy()
     }
 
+
 }
